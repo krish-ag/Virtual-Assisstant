34a93624784ebeac61d1d019ad9e209f793ffc43
krish-ag <63946679+krish-ag@users.noreply.github.com>
2020-10-01 09:27:20 +0530

    improved VA.py
    cleared some issues with your code

--- VA.py
+++ VA.py
@@ -1,36 +1,39 @@
 import wolframalpha # Wolfram Alpha API (THE BRAINS!!!)
 import pyttsx3 # Speech Synthesis
-import speech_recognition as sr 
-import random 
-from playsound import playsound # Media Player 
+import speech_recognition as sr
+import random
+from playsound import playsound # Media Player
 
 r = sr.Recognizer()
 engine = pyttsx3.init()
-rate = engine.getProperty('rate')
-engine.setProperty('rate', 190)
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+rate = engine.getProperty("rate")
+engine.setProperty("rate", 190)
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
 app_id = "Q52YQA-HQH6T3GK9J"
 
 def main():
     with sr.Microphone() as source:
-        ans = ["What's up?", "What do you need?", "What can I do for you?"] # Catalog of phrases after trigger 
+        ans = ["What's up?", "What do you need?", "What can I do for you?"] # Catalog of phrases after trigger
         wait = ["Give me a second while I think.", "Ok thinking"] # Catalog of phrases after command
         random_ans = random.choice(ans) # Randomizes answer phrase choice
         random_wait = random.choice(wait) #  Randomizes wait phrase choice
         try:
             r.adjust_for_ambient_noise(source) # Adjusts audio input for improved speech recognition
+            print("Listening...") # to understand when the program(virtual assistant) is listening.
             audio = r.listen(source) # Listens for voice
             if r.recognize_google(audio) == "hey Sarah" or "Hey sarah" or "Hey Sarah" or "hey sarah": # Trigger (What the Assistant answer to) // Also checks for trigger
-                playsound('ding.mp3') 
+                playsound("ding.mp3")
                 engine.say(random_ans)
                 print("Sarah: "+random_ans)
                 engine.runAndWait()
                 audio = r.listen(source)
                 if r.recognize_google(audio) == "nevermind":
                     print("You: "+r.recognize_google(audio))
-                    engine.say("Ok Bye.")
-                    print("Sarah: "+"Ok Bye.")
+                    bye = ["Bye...","see You Next Time...","Good Bye For Now..."]
+                    random_bye = random.choice(bye)
+                    engine.say(random_bye)
+                    print("Sarah: OK, " + random_bye)
                     engine.runAndWait()
                     main()
                 else:
